e8749be7af7076498278863cb27bedba3821cf0b
Dan-Koyuki <dankoyuki398@gmail.com>
2024-01-26 15:24:46 +0700

    refactoring Pokemon.js function

--- src/functions/tests/pokedex.test.js
+++ src/functions/tests/pokedex.test.js
@@ -1,21 +1,16 @@
 // pokedex.test.js (in src/functions/tests)
 import {
-  findByATKStat,
   findByAbility,
-  findByDEFStat,
   findByEggGroup,
-  findByHPStat,
   findByHiddenAbility,
   findById,
   findByMaleRatio,
   findByName,
   findByRegion,
-  findBySPEStat,
-  findBySpAStat,
-  findBySpDStat,
-  findByStandardAbility,
+  findByNormalAbility,
   findByType,
   findByTypes,
+  findByBaseStat,
 } from "../utils/Pokemon.js";
 
 // Test Usage
@@ -69,65 +64,12 @@ console.log(
   pokemonDA ? `True, ${pokemonDA.length}` : "False"
 );
 
-const pokemonDB = findByStandardAbility('blaze');
+const pokemonDB = findByNormalAbility('blaze');
 console.log(
   "Is Pokemons Exist By Ability? ",
   pokemonDB ? `True, ${pokemonDB.length}` : "False"
 );
 
-// Search By HP Stat {check}
-const pokemonE = findByHPStat(100);
-// const pokemons = pokemonE.map((poke) => poke.name);
-// console.log(pokemons);
-console.log(
-  "Is Pokemon Exist By HP Stat? ",
-  pokemonE ? `True, ${pokemonE.length}` : "False"
-);
-
-// Search By ATK Stat {check}
-const pokemonF = findByATKStat(100);
-// const pokemonsF = pokemonF.map((poke) => poke.name);
-// console.log(pokemonsF);
-console.log(
-  "Is Pokemon Exist By Atk Stat? ",
-  pokemonF ? `True, ${pokemonF.length}` : "False"
-);
-
-// Search By DEF Stat {check}
-const pokemonG = findByDEFStat(100);
-// const pokemonsG = pokemonG.map((poke) => poke.name);
-// console.log(pokemonsG);
-console.log(
-  "Is Pokemon Exist By Def Stat? ",
-  pokemonG ? `True, ${pokemonG.length}` : "False"
-);
-
-// Search By SpA Stat {check}
-const pokemonH = findBySpAStat(100);
-// const pokemonsH = pokemonH.map((poke) => poke.name);
-// console.log(pokemonsH);
-console.log(
-  "Is Pokemon Exist By SpA Stat? ",
-  pokemonH ? `True, ${pokemonH.length}` : "False"
-);
-
-// Search By SpD Stat {check}
-const pokemonI = findBySpDStat(100);
-// const pokemonsI = pokemonI.map((poke) => poke.name);
-// console.log(pokemonsI);
-console.log(
-  "Is Pokemon Exist By SpD Stat? ",
-  pokemonI ? `True, ${pokemonI.length}` : "False"
-);
-
-// Search By Speed Stat {check}
-const pokemonJ = findBySPEStat(100);
-// const pokemonsJ = pokemonJ.map((poke) => poke.name);
-// console.log(pokemonsJ);
-console.log(
-  "Is Pokemon Exist By Speed Stat? ",
-  pokemonJ ? `True, ${pokemonJ.length}` : "False"
-);
 
 const pokemonK = findByEggGroup("monster");
 // const pokemons = pokemonK.map((poke) => poke.name);
@@ -138,11 +80,19 @@ console.log(
 );
 
 const pokemonL = findByMaleRatio(87.5);
-const pokemons = pokemonL.map((poke) => poke.name);
-console.log(pokemons);
+// const pokemons = pokemonL.map((poke) => poke.name);
+// console.log(pokemons);
 console.log(
   "Is Pokemon Exist By Male Ratio? ", 
   pokemonL ? `True, ${pokemonL.length}` : "False"
 );
 // const pokemons = pokemonD?.map(poke => poke.name);
 // console.log(pokemons)
+
+const pokemonM = findByBaseStat(100, 'atk');
+const pokemonsM = pokemonM.map((poke) => poke.name);
+console.log(pokemonsM);
+console.log(
+  "Is Pokemon Exist By Male Ratio? ", 
+  pokemonM ? `True, ${pokemonM.length}` : "False"
+);
--- src/functions/utils/Pokemon.js
+++ src/functions/utils/Pokemon.js
@@ -7,14 +7,14 @@ import { capitalize } from "./helper.js";
  * @returns {object|null} - Pokemon object with the matching ID, or null if not found.
  */
 export const findById = (id) => {
-  if (typeof id !== 'string' || id.length !== 4 || isNaN(id.toString())) {
-    throw new Error('Invalid ID format. Please provide a 4-digit numeric ID.');
+  if (typeof id !== "string" || id.length !== 4 || isNaN(id.toString())) {
+    throw new Error("Invalid ID format. Please provide a 4-digit numeric ID.");
   }
 
   const res = Pokedex.find((pokemon) => pokemon.id === id);
 
   if (!res || res.length === 0) {
-    throw new Error('No Pokemon Found!');
+    throw new Error("No Pokemon Found!");
   }
 
   return res;
@@ -26,14 +26,16 @@ export const findById = (id) => {
  * @returns {object|null} - Pokemon object with the matching Name, or null if not found.
  */
 export const findByName = (name) => {
-  if (typeof name !== 'string'){
-    throw new Error("Invalid Pokemon Name. Please Provide a correct Pokemon Name.");
+  if (typeof name !== "string") {
+    throw new Error(
+      "Invalid Pokemon Name. Please Provide a correct Pokemon Name."
+    );
   }
   const req = capitalize(name);
   const res = Pokedex.find((pokemon) => pokemon.name === req);
 
-  if (!res || res.length === 0){
-    throw new Error('No Pokemon Found!');
+  if (!res || res.length === 0) {
+    throw new Error("No Pokemon Found!");
   }
 
   return res;
@@ -45,16 +47,29 @@ export const findByName = (name) => {
  * @returns {Array} - array of Pokemon object with the matchin Region.
  */
 export const findByRegion = (region) => {
-  const Region = ['kanto', 'johto', 'hoenn', 'sinnoh', 'unova', 'kalos', 'alola', 'hisui', 'galar', 'paldea'];
-  if (typeof region !== 'string' || !Region.includes(region.toLowerCase())){
-    throw new Error("Invalid Region Name. Please Provide a correct Region Name.");
+  const Region = [
+    "kanto",
+    "johto",
+    "hoenn",
+    "sinnoh",
+    "unova",
+    "kalos",
+    "alola",
+    "hisui",
+    "galar",
+    "paldea",
+  ];
+  if (typeof region !== "string" || !Region.includes(region.toLowerCase())) {
+    throw new Error(
+      "Invalid Region Name. Please Provide a correct Region Name."
+    );
   }
   const req = capitalize(region);
   const res = Pokedex.filter((pokemon) => {
     return pokemon.region === req;
   });
 
-  if (!res || res.length === 0){
+  if (!res || res.length === 0) {
     throw new Error(`No Pokemon Found from ${region}!`);
   }
 
@@ -67,8 +82,27 @@ export const findByRegion = (region) => {
  * @returns {Array} - array of Pokemon object with matching Type.
  */
 export const findByType = (type) => {
-  const valid = ['fire', 'normal', 'grass', 'water', 'electric', 'ground', 'rock', 'ice', 'steel', 'dragon', 'dark', 'ghost', 'bug', 'fighting', 'flying', 'psychic', 'fairy', 'poison'];
-  if (typeof type !== 'string' || !valid.includes(type)){
+  const valid = [
+    "fire",
+    "normal",
+    "grass",
+    "water",
+    "electric",
+    "ground",
+    "rock",
+    "ice",
+    "steel",
+    "dragon",
+    "dark",
+    "ghost",
+    "bug",
+    "fighting",
+    "flying",
+    "psychic",
+    "fairy",
+    "poison",
+  ];
+  if (typeof type !== "string" || !valid.includes(type)) {
     throw new Error("Invalid Type! Please provide a correct Type of Pokemon");
   }
   const req = capitalize(type);
@@ -76,7 +110,7 @@ export const findByType = (type) => {
     return pokemon.types.includes(req);
   });
 
-  if (!res || res.length === 0){
+  if (!res || res.length === 0) {
     throw new Error(`No Pokemon with ${type} type!`);
   }
 
@@ -89,10 +123,31 @@ export const findByType = (type) => {
  * @returns {Array} - Array of Pokemon objects with matching type combination.
  */
 export const findByTypes = (types) => {
-  const valid = ['fire', 'normal', 'grass', 'water', 'electric', 'ground', 'rock', 'ice', 'steel', 'dragon', 'dark', 'ghost', 'bug', 'fighting', 'flying', 'psychic', 'fairy', 'poison'];
+  const valid = [
+    "fire",
+    "normal",
+    "grass",
+    "water",
+    "electric",
+    "ground",
+    "rock",
+    "ice",
+    "steel",
+    "dragon",
+    "dark",
+    "ghost",
+    "bug",
+    "fighting",
+    "flying",
+    "psychic",
+    "fairy",
+    "poison",
+  ];
 
   if (!Array.isArray(types) || !types.every((type) => valid.includes(type))) {
-    throw new Error("Invalid Type of Pokemon! Please provide a correct Type of Pokemon");
+    throw new Error(
+      "Invalid Type of Pokemon! Please provide a correct Type of Pokemon"
+    );
   }
 
   const req = types.map((type) => capitalize(type));
@@ -101,7 +156,7 @@ export const findByTypes = (types) => {
   });
 
   if (!res || res.length === 0) {
-    throw new Error(`No Pokemon with combination type of ${types.join(', ')}!`);
+    throw new Error(`No Pokemon with combination type of ${types.join(", ")}!`);
   }
 
   return res;
@@ -112,10 +167,12 @@ export const findByTypes = (types) => {
  * @param {string} ability - name of Ability (e.g., 'adaptability', 'Air Lock').
  * @returns {Array} array of Pokemon objects with the matching Ability.
  */
-// findByAbility => Find some Pokemon that having the Ability.
-// @param ability - ability of the Pokemon (e.g., 'adaptability', 'Air Lock').
-// @return array of Pokemon objects with the matching ability, or null if not found.
 export const findByAbility = (ability) => {
+  if (typeof ability !== "string") {
+    throw new Error(
+      "Invalid Ability Name. Please provide a correct Ability Name."
+    );
+  }
   const req = capitalize(ability);
   const res = Pokedex.filter((pokemon) => {
     return (
@@ -123,197 +180,134 @@ export const findByAbility = (ability) => {
     );
   });
 
-  if (!res || res.length === 0){
-    console.log("No Pokemon Found!")
-    console.log("Check Given Parameters!")
+  if (!res || res.length === 0) {
+    throw new Error(`No Pokemon with ${ability} is found!`);
   }
 
-  return res || null;
+  return res;
 };
 
-// findByHiddenAbility => Find some Pokemon that having the Ability as Hidden Ability.
-// @param ability - ability of the Pokemon (e.g., 'adaptability', 'Air Lock').
-// @return array of Pokemon objects with the matching Hidden ability, or null if not found.
+/**
+ * findByHiddenAbility - Find all Pokemon with certain Hidden Ability.
+ * @param {string} ability - name of Hidden Ability (e.g., 'adaptability', 'Air Lock').
+ * @returns {Array} array of Pokemon objects with the matching Hidden Ability.
+ */
 export const findByHiddenAbility = (ability) => {
+  if (typeof ability !== "string") {
+    throw new Error(
+      "Invalid Ability Name. Please provide a correct Ability Name."
+    );
+  }
   const req = capitalize(ability);
   const res = Pokedex.filter((pokemon) => {
     return pokemon.abilities.hidden === req;
   });
 
-  if (!res || res.length === 0){
-    console.log("No Pokemon Found!")
-    console.log("Check Given Parameters!")
+  if (!res || res.length === 0) {
+    throw new Error(`No Pokemon have ${ability} as Hidden Ability!`);
   }
 
-  return res || null;
-}
+  return res;
+};
 
-// findByStandardAbility => Find some Pokemon that having the Ability as Standard Ability.
-// @param ability - ability of the Pokemon (e.g., 'adaptability', 'Air Lock').
-// @return array of Pokemon objects with the matching Standard ability, or null if not found.
-export const findByStandardAbility = (ability) => {
+/**
+ * findByNormalAbility - Find all Pokemon with certain Normal Ability.
+ * @param {string} ability - name of Hidden Ability (e.g., 'adaptability', 'Air Lock').
+ * @returns {Array} array of Pokemon objects with the matching Normal Ability.
+ */
+export const findByNormalAbility = (ability) => {
+  if (typeof ability !== "string") {
+    throw new Error(
+      "Invalid Ability Name. Please provide a correct Ability Name."
+    );
+  }
   const req = capitalize(ability);
   const res = Pokedex.filter((pokemon) => {
     return pokemon.abilities.normal.includes(req);
   });
 
-  if (!res || res.length === 0){
-    console.log("No Pokemon Found!")
-    console.log("Check Given Parameters!")
+  if (!res || res.length === 0) {
+    throw new Error(`No Pokemon have ${ability} as Normal Ability!`);
   }
 
-  return res || null;
-}
-
-// findByHPStat => Find some Pokemon that having the Base HP.
-// @param HpStat - Base HP Stat of the Pokemon.
-// @return array of Pokemon objects with matching Base HP, or null if not found.
-export const findByHPStat = (HpStat) => {
-  const req = HpStat.toString();
-  const res = Pokedex.filter((pokemon) => {
-    return pokemon.stats.hp === req;
-  });
-
-  if (!res || res.length === 0){
-    console.log("No Pokemon Found!")
-    console.log("Check Given Parameters!")
-  }
-
-  return res || null;
+  return res;
 };
 
-// findByATKStat => Find some Pokemon that having the Base ATK.
-// @param ATKStat - Base ATK Stat of the Pokemon.
-// @return array of Pokemon objects with matching Base ATK, or null if not found.
-export const findByATKStat = (ATKStat) => {
-  const req = ATKStat.toString();
+/**
+ * findByBaseStat - Find all Pokemon that have the specified base stat.
+ * @param {number} stat - Base stat of the Pokemon to search for.
+ * @param {string} statType - Type of base stat ('hp', 'atk', 'def', 'spa', 'spd', 'speed').
+ * @return {Array} - Array of Pokemon objects with matching base stat.
+ */
+export const findByBaseStat = (stat, statType) => {
+  const valid = ["hp", "atk", "def", "spa", "spd", "speed"];
+  if (typeof stat !== "string" && typeof stat !== "number") {
+    throw new Error(
+      "Invalid Stat Format! Stat should be a number or a string."
+    );
+  }
+  if (typeof statType !== "string" || !valid.includes(statType.toLowerCase())) {
+    throw new Error(
+      "Invalid Stat Type! StatType should be one of 'hp', 'atk', 'def', 'spa', 'spd', 'speed'."
+    );
+  }
+  const req = stat.toString();
   const res = Pokedex.filter((pokemon) => {
-    return pokemon.stats.atk === req;
+    return pokemon.stats[statType] === req;
   });
 
-  if (!res || res.length === 0){
-    console.log("No Pokemon Found!")
-    console.log("Check Given Parameters!")
+  if (!res || res.length === 0) {
+    throw new Error(`No Pokemon found with ${statType} base stat of ${stat}.`);
   }
 
-  return res || null;
+  return res;
 };
 
-// findByDEFStat => Find some Pokemon that having the Base DEF.
-// @param DEFStat - Base DEF Stat of the Pokemon.
-// @return array of Pokemon objects with matching Base DEF, or null if not found.
-export const findByDEFStat = (DEFStat) => {
-  const req = DEFStat.toString();
-  const res = Pokedex.filter((pokemon) => {
-    return pokemon.stats.def === req;
-  });
-
-  if (!res || res.length === 0){
-    console.log("No Pokemon Found!")
-    console.log("Check Given Parameters!")
-  }
-
-  return res || null;
-};
-
-// findBySpAStat => Find some Pokemon that having the Base SpAtk.
-// @param SpAStat - Base SpAtk Stat of the Pokemon.
-// @return array of Pokemon objects with matching Base SpAtk, or null if not found.
-export const findBySpAStat = (SpAStat) => {
-  const req = SpAStat.toString();
-  const res = Pokedex.filter((pokemon) => {
-    return pokemon.stats.spa === req;
-  });
-
-  if (!res || res.length === 0){
-    console.log("No Pokemon Found!")
-    console.log("Check Given Parameters!")
-  }
-
-  return res || null;
-};
-
-// findBySpDStat => Find some Pokemon that having the Base SpDef.
-// @param SpDStat - Base SpDef Stat of the Pokemon.
-// @return array of Pokemon objects with matching Base SpDef, or null if not found.
-export const findBySpDStat = (SpDStat) => {
-  const req = SpDStat.toString();
-  const res = Pokedex.filter((pokemon) => {
-    return pokemon.stats.spd === req;
-  });
-
-  if (!res || res.length === 0){
-    console.log("No Pokemon Found!")
-    console.log("Check Given Parameters!")
-  }
-
-  return res || null;
-};
-
-// findBySPEStat => Find some Pokemon that having the Base Speed.
-// @param SPEStat - Base Speed Stat of the Pokemon.
-// @return array of Pokemon objects with matching Base Speed, or null if not found.
-export const findBySPEStat = (SPEStat) => {
-  const req = SPEStat.toString();
-  const res = Pokedex.filter((pokemon) => {
-    return pokemon.stats.speed === req;
-  });
-
-  if (!res || res.length === 0){
-    console.log("No Pokemon Found!")
-    console.log("Check Given Parameters!")
-  }
-
-  return res || null;
-};
-
-// findByEggGroup => Find some Pokemon with certain Egg Group.
-// @param eggGroup - Egg Group of the Pokemon (e.g., 'monster', 'grass', 'dragon', 'field').
-// @return array of Pokemon objects with matching Egg Group.
+/**
+ * findByEggGroup - Find all Pokemon with certain Egg Group
+ * @param {eggGroup} eggGroup -  Egg Group of the Pokemon (e.g., 'monster', 'grass', 'dragon', 'field').
+ * @returns {Array} - Array of Pokemon object with matching Egg Group.
+ */
 export const findByEggGroup = (eggGroup) => {
+  const valid = ['monster', 'human-like', 'water 1', 'water 3', 'bug', 'mineral', 'flying', 'amorphous', 'field', 'water 2', 'fairy', 'ditto', 'grass', 'dragon', 'no eggs discovered', 'gender unknown'];
+  if (typeof eggGroup !== 'string' || !valid.includes(eggGroup.toLowerCase())){
+    throw new Error("Invalid Egg Group! Egg Group should be one of 'monster', 'human-like', 'water 1', 'water 3', 'bug', 'mineral', 'flying', 'amorphous', 'field', 'water 2', 'fairy', 'ditto', 'grass', 'dragon' or 'no eggs discovered'!");
+  }
   const req = capitalize(eggGroup);
   const res = Pokedex.filter((pokemon) => {
     return pokemon.eggGroups.includes(req);
-  })
+  });
 
-  if (!res || res.length === 0){
-    console.log("No Pokemon Found!")
-    console.log("Check Given Parameters!")
+  if (!res || res.length === 0) {
+    throw new Error(`No Pokemon from ${eggGroup} Egg Groups!`);
   }
 
-  return res || null;
-}
+  return res;
+};
 
-// findByMaleRatio => Find some Pokemon with certain Male Ratio.
-// @param ratio - Gender Ratio of the Pokemon.
-// @return array of Pokemon objects with matching Male Ratio.
-export const findByMaleRatio = (ratio) => {
-  const req = parseFloat(ratio); // just to make sure it was integer
+/**
+ * findByGenderRatio - Find all Pokemon that have the specified Gender Ratio.
+ * @param {number} ratio - Gender Ratio of the Pokemon.
+ * @param {string} gender - Gender of the Pokemon ('male' or 'female').
+ * @returns array of Pokemon objects with matching Ratio.
+ */
+export const findByGenderRatio = (ratio, gender) => {
+  const valid = ['male', 'female'];
+  if (typeof ratio !== 'number'){
+    throw new Error("Invalid Gender Ratio Format! Gender Ratio should be a number!");
+  }
+  if (typeof gender !== 'string' || !valid.includes(gender.toLowerCase())){
+    throw new Error("Invalid Gender of Pokemon! Gender should be 'male' or 'female'!");
+  }
+  const req = parseFloat(ratio);
+  const type = gender.toLowerCase() === 'male' ? 0 : 1;
   const res = Pokedex.filter((pokemon) => {
-    return pokemon.genderRatios[0] === req;
+    return pokemon.genderRatios[type] === req;
   })
 
   if (!res || res.length === 0){
-    console.log("No Pokemon Found!")
-    console.log("Check Given Parameters!")
+    throw new Error(`No Pokemon with ${gender} ratio of ${ratio}!`);
   }
 
-  return res || null;
-}
-
-// findByFemaleRatio => Find some Pokemon with certain Female Ratio.
-// @param ratio - Gender Ratio of the Pokemon.
-// @return array of Pokemon objects with matching Female Ratio.
-export const findByFemaleRatio = (ratio) => {
-  const req = parseFloat(ratio); // just to make sure it was integer
-  const res = Pokedex.filter((pokemon) => {
-    return pokemon.genderRatios[1] === req;
-  })
-
-  if (!res || res.length === 0){
-    console.log("No Pokemon Found!")
-    console.log("Check Given Parameters!")
-  }
-
-  return res || null;
+  return res;
 }
